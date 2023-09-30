6fb8ad8de32a6d6885f571c42bb445632e6d648b
cheolwon1994 <chlcjfahs@naver.com>
2023-09-30 10:58:46 +0900

    Add test codes for UserApiController

--- src/test/java/com/catcher/app/AppApplicationTests.java
+++ src/test/java/com/catcher/app/AppApplicationTests.java
@@ -2,8 +2,10 @@ package com.catcher.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan(basePackages = {"com.catcher.resource"})
 class AppApplicationTests {
 
 	@Test
