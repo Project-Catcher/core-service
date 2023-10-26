caf0743a8d22340ab480c71ddb88ee12db78e6d7
jmcunst <xnslqjtmghaf@gmail.com>
2023-10-26 13:46:58 +0900

    feat : base exception

---
+++ src/main/java/com/catcher/common/response/BaseResponse.java
@@ -0,0 +1,48 @@
+package com.catcher.common.response;
+
+import com.catcher.common.BaseResponseStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.catcher.common.BaseResponseStatus.SUCCESS;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class BaseResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final int code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.code = SUCCESS.getCode();
+        this.message = SUCCESS.getMessage();
+        this.result = result;
+    }
+
+    public BaseResponse(BaseResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+    }
+
+    public BaseResponse(String message){
+        this.isSuccess = false;
+        this.code = 5000;
+        this.message = message;
+    }
+
+    public BaseResponse(BaseResponseStatus status, T result){
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+        this.result = result;
+    }
+}
