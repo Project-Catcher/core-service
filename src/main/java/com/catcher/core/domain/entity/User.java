c5680da790fe219c04247d43657cf62ee26fd5fa
jmcunst <xnslqjtmghaf@gmail.com>
2023-10-26 13:48:09 +0900

    update : jwt

--- src/main/java/com/catcher/core/domain/entity/User.java
+++ src/main/java/com/catcher/core/domain/entity/User.java
@@ -4,6 +4,7 @@ import com.catcher.core.domain.entity.enums.UserProvider;
 import com.catcher.core.domain.entity.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,16 +21,28 @@ public class User extends BaseTimeEntity {
     private Long id;
 
     @Column(unique = true, nullable = false)
+    private String uid;
+
+    @Column(unique = true, nullable = false)
     private String username;
 
     // TODO: encrypted maybe?
     @Column(nullable = false)
     private String password;
 
+    @Column(unique = true, nullable = false)
     private String phone;
+
+    @Column(unique = true, nullable = false)
+    private String email;
 
     // TODO : 첨부파일 외래키 참조해야 지
     private String profileImageUrl;
+
+    private String introduceContent;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
 
     @Enumerated(value = EnumType.STRING)
     private UserProvider userProvider;
@@ -38,14 +51,33 @@ public class User extends BaseTimeEntity {
     private UserRole userRole;
 
     @Column(nullable = false) // 필수 약관이라 가정
-    private LocalDateTime userTerm1;
+    private LocalDateTime userAgeTerm;
 
-    private LocalDateTime userTerm2; // 선택 약관이라 가정
+    private LocalDateTime userServiceTerm; // 선택 약관이라 가정
 
-    private LocalDateTime userTerm3;
+    private LocalDateTime userPrivacyTerm;
 
-    private LocalDateTime userTerm4;
+    private LocalDateTime userLocationTerm;
+
+    private LocalDateTime userMarketingTerm;
 
     private LocalDateTime deletedAt;
 
+    @Builder
+    public User(String uid, String password, String username, String email, String profileImageUrl, String phone, String nickname, UserRole role, LocalDateTime userAgeTerm, LocalDateTime userServiceTerm, LocalDateTime userPrivacyTerm, LocalDateTime userLocationTerm, String introduceContent, LocalDateTime userMarketingTerm){
+        this.uid = uid;
+        this.password = password;
+        this.username = username;
+        this.email = email;
+        this.profileImageUrl = profileImageUrl;
+        this.phone = phone;
+        this.nickname = nickname;
+        this.userRole = role;
+        this.userAgeTerm = userAgeTerm;
+        this.userServiceTerm = userServiceTerm;
+        this.userPrivacyTerm = userPrivacyTerm;
+        this.userLocationTerm = userLocationTerm;
+        this.introduceContent = introduceContent;
+        this.userMarketingTerm = userMarketingTerm;
+    }
 }
--- src/main/java/com/catcher/core/service/UserService.java
+++ src/main/java/com/catcher/core/service/UserService.java
@@ -1,12 +1,107 @@
 package com.catcher.core.service;
 
-import jakarta.transaction.Transactional;
+import com.catcher.common.exception.BaseException;
+import com.catcher.config.JwtTokenProvider;
+import com.catcher.core.domain.entity.enums.UserRole;
+import com.catcher.core.dto.TokenDto;
+import com.catcher.core.dto.user.UserCreateReqDto;
+import com.catcher.core.dto.user.UserCreateResDto;
+import com.catcher.core.dto.user.UserLoginReqDto;
+import com.catcher.core.dto.user.UserResDto;
+import com.catcher.core.domain.entity.User;
+import com.catcher.datasource.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import java.util.Optional;
+
+import static com.catcher.common.BaseResponseStatus.*;
+
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
+@Service
+@Log4j2
 public class UserService {
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserCreateResDto signUpUser(UserCreateReqDto userCreateReqDto) throws BaseException {
+        validateDuplicateUser(userCreateReqDto.getUid());
+
+        User user = User.builder()
+                .uid(userCreateReqDto.getUid())
+                .password(passwordEncoder.encode(userCreateReqDto.getPassword()))
+                .role(userCreateReqDto.getRole() == 0 ? UserRole.ADMIN : UserRole.USER)
+                .username(userCreateReqDto.getUsername())
+                .email(userCreateReqDto.getEmail())
+                .phone(userCreateReqDto.getPhone())
+                .nickname(userCreateReqDto.getNickname())
+                .profileImageUrl(null)
+                .userAgeTerm(userCreateReqDto.getAgeTerm())
+                .userServiceTerm(userCreateReqDto.getServiceTerm())
+                .userPrivacyTerm(userCreateReqDto.getPrivacyTerm())
+                .userLocationTerm(userCreateReqDto.getLocationTerm())
+                .userMarketingTerm(userCreateReqDto.getMarketingTerm())
+                .introduceContent(userCreateReqDto.getIntroduceContent())
+                .build();
+
+        userRepository.save(user);
+
+        return UserCreateResDto.from(user);
+    }
+
+
+    // 유저 중복 확인
+    private void validateDuplicateUser(String uid) throws BaseException {
+        Optional<User> findUsers = userRepository.findByUid(uid);
+        if (!findUsers.isEmpty()){
+            throw new BaseException(USERS_DUPLICATED_USER_ID);
+        }
+    }
+
+    @Transactional
+    public TokenDto login(UserLoginReqDto userLoginReqDto) throws BaseException {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            userLoginReqDto.getUid(),
+                            userLoginReqDto.getPassword()
+                    )
+            );
+
+            TokenDto tokenDto = new TokenDto(
+                    jwtTokenProvider.createAccessToken(authentication),
+                    jwtTokenProvider.createRefreshToken(authentication)
+            );
+
+            return tokenDto;
+
+        }catch(BadCredentialsException e){
+            log.error(INVALID_USER_PW.getMessage());
+            throw new BaseException(INVALID_USER_PW);
+        }
+    }
+
+    public UserResDto getUser(String uid) {
+        Optional<User> users = userRepository.findByUid(uid);
+        User user = users.orElseThrow(() -> {
+            log.error(INVALID_USER_UID.getMessage());
+            return new BaseException(INVALID_USER_UID);
+        });
+
+        return UserResDto.from(user);
+    }
 }
