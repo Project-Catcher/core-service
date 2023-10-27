0475caae0e44c175ef3001214189957f647ea699
jmcunst <xnslqjtmghaf@gmail.com>
2023-10-28 08:38:18 +0900

    feat : jwt customization

---
+++ src/main/java/com/catcher/config/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.catcher.config;
+
+import com.catcher.common.exception.BaseException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static com.catcher.common.BaseResponseStatus.INVALID_JWT;
+import static com.catcher.common.BaseResponseStatus.REDIS_ERROR;
+
+/**
+ * 헤더(Authorization)에 있는 토큰을 꺼내 이상이 없는 경우 SecurityContext에 저장
+ * Request 이전에 작동
+ */
+
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth); // 정상 토큰이면 SecurityContext에 저장
+            }
+        } catch (RedisConnectionFailureException e) {
+            SecurityContextHolder.clearContext();
+            throw new BaseException(REDIS_ERROR);
+        } catch (Exception e) {
+            throw new BaseException(INVALID_JWT);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
