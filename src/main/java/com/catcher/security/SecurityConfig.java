cebc303855d0a2adab6265a7aba01d90ec7d97c1
jmcunst <xnslqjtmghaf@gmail.com>
2023-10-28 08:39:40 +0900

    feat : security settings

---
+++ src/main/java/com/catcher/security/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.catcher.security;
+
+import com.catcher.config.JwtAccessDeniedHandler;
+import com.catcher.config.JwtAuthenticationEntryPoint;
+import com.catcher.config.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    private final String[] allowedUrls = {"/", "/swagger-ui/**", "/v3/**", "/api/users/login", "/api/users/signup", "/access-test"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity
+                .csrf((csrf) -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()))
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .requestMatchers(allowedUrls).permitAll()
+                                .requestMatchers("/api/**").permitAll()
+                                .requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(
+                        (exceptionHandling) ->
+                                exceptionHandling
+                                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                ).apply(new JwtSecurityConfig(jwtTokenProvider));
+
+        return httpSecurity.build();
+    }
+}
