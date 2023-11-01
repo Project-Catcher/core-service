496174ddfde965d570abfa13ed24630753096d2e
HongGeun <ghdrms163@gmail.com>
2023-11-01 15:40:46 +0900

    feat: add user find by email

--- src/main/java/com/catcher/datasource/UserRepository.java
+++ src/main/java/com/catcher/datasource/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(final String username);
 
     Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(final String email);
 }
