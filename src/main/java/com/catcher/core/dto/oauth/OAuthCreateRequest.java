e941213803a4a6af0ccaca263bb39689925f8cd0
HongGeun <ghdrms163@gmail.com>
2023-11-01 16:58:15 +0900

    refactor: change oauth sign dto format

--- src/main/java/com/catcher/core/dto/oauth/OAuthCreateRequest.java
+++ src/main/java/com/catcher/core/dto/oauth/OAuthCreateRequest.java
@@ -1,7 +1,9 @@
 package com.catcher.core.dto.oauth;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -14,15 +16,19 @@ public class OAuthCreateRequest {
     private String nickname;
 
     @NotNull(message = "필수 약관 14세 이상 동의해주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime ageTerm;
 
     @NotNull(message = "필수 약관 서비스 이용 동의해주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime serviceTerm;
 
     @NotNull(message = "필수 약관 개인정보 이용 동의해주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime privacyTerm;
 
     @NotNull(message = "필수 약관 위치 정보 이용 동의해주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime locationTerm;
 
     @NotNull(message = "액세스 토큰이 없습니다.")
@@ -30,6 +36,7 @@ public class OAuthCreateRequest {
 
     private int role;
     private String introduceContent;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime marketingTerm;
 
 }
