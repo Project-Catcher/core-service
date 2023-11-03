daf5aaead20b8d48f2f4f9766a22f68dc81a5190
HongGeun <ghdrms163@gmail.com>
2023-11-04 02:04:36 +0900

    divide userrepo

--- src/main/java/com/catcher/datasource/UserJpaRepository.java
+++ src/main/java/com/catcher/datasource/UserJpaRepository.java
@@ -3,6 +3,9 @@ package com.catcher.datasource;
 import com.catcher.core.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository {
+import java.util.Optional;
 
+interface UserJpaRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
