8f226292251b9f15afc21d5f6e9f0e4dbef40090
HongGeun <ghdrms163@gmail.com>
2023-11-04 01:45:46 +0900

    remove not used in servlitutills

--- src/main/java/com/catcher/utils/HttpServletUtils.java
+++ src/main/java/com/catcher/utils/HttpServletUtils.java
@@ -21,33 +21,4 @@ public final class HttpServletUtils {
     public static void putHeader(HttpServletResponse response, String name, String value) {
         response.setHeader(name, value);
     }
-
-    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
-        Cookie[] cookies = request.getCookies();
-
-        if (cookies != null && cookies.length > 0) {
-            return Arrays.stream(cookies)
-                    .filter(cookie -> cookie.getName().equals(name))
-                    .findAny();
-        }
-
-        return empty();
-    }
-
-    public static void addCookie(HttpServletResponse response, String name, String value, int seconds) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(seconds);
-        response.addCookie(cookie);
-    }
-
-    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
-        getCookie(request, name).ifPresent(cookie -> {
-            cookie.setValue("");
-            cookie.setPath("/");
-            cookie.setMaxAge(0);
-            response.addCookie(cookie);
-        });
-    }
 }
