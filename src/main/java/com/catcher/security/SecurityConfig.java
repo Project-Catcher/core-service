4f06d46569daa1c30ccce92c8515e3b59c2817b9
HongGeun <ghdrms163@gmail.com>
2023-11-20 01:36:25 +0900

    authentication 구조 변경 및 커스텀 CatcherUser

--- src/main/java/com/catcher/security/SecurityConfig.java
+++ src/main/java/com/catcher/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -39,9 +39,9 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration
+            UserDetailServiceImpl userDetailService, PasswordEncoder passwordEncoder
     ) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+        return new AuthenticationProxy(userDetailService, passwordEncoder);
     }
 
     @Bean
