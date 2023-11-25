9599b7f000f724066f1e9a0b5367a300446f7f79
HongGeun <ghdrms163@gmail.com>
2023-11-25 20:00:13 +0900

    edit default contstructor

--- src/main/java/com/catcher/common/response/CommonResponse.java
+++ src/main/java/com/catcher/common/response/CommonResponse.java
@@ -3,14 +3,16 @@ package com.catcher.common.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CommonResponse<T> {
-    private final int code;
-    private final boolean success;
+    private int code;
+    private boolean success;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private final T result;
+    private T result;
 
     @Builder
     public CommonResponse(int code, boolean success, T result) {
